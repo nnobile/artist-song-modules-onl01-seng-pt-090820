dff1b38e08030324dd8c7efb679ec0ef95a4180c
Nick Nobile <nickcnobile@gmail.com>
2020-10-12 03:10:51 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable
 
   def self.find_by_name(name)
-    Song.find_by_name.detect{|a| a.name == name}
+    self.find_by_name.detect{|a| a.name == name}
     Artist.find_by_name.detect{|a| a.name == name}
     #self.detect{|a| a.name == name}
   end
